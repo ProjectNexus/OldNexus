72a5d4aac3d61ec7570a2b22c52240b551fa4934
hamilr2 <hamilr2@rpi.edu>
2011-06-10 14:08:39 -0400

    Added Devise, pages controller. Massive scaffolding to start soon...

--- Gemfile
+++ Gemfile
@@ -7,6 +7,10 @@ gem 'rails', '3.0.5'
 
 gem 'sqlite3'
 
+gem 'devise'
+
+gem 'rake', "!= 0.9.0"
+
 # Use unicorn as the web server
 # gem 'unicorn'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Nexus::Application.routes.draw do
+  root :to => "pages#home"
+
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
